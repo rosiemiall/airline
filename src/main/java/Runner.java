681c113b85ce08c7d5a9f168959289145cd74242
RyanNekadio <ryanekadio12@hotmail.co.uk>
2023-10-24 16:16:12 +0100

    Uploading completed MVP tasks

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 
@@ -36,10 +37,71 @@ public class Runner {
         System.out.println("Contact Number: " + passenger.getContactInfo());
         System.out.println("Unique ID: " + passenger.getUniqueID());
 
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Please input a destination you would like to travel: ");
+        String destinationName = scanner1.nextLine();
+        if (destinationName.equals(destination)){
+            airline.book(passenger, newFlight);
+            System.out.println("Passengers on the flight to " + newFlight.getDestination());
+            for (Passenger person : newFlight.getPassengers()){
+                System.out.println("Passenger name: " + passenger.getName() +
+                        " |  Contact number: " + passenger.getContactInfo() +
+                        " | Unique ID: " + passenger.getUniqueID());
+            }
+        } else if (destinationName.equals("New York")) {
+            airline.book(passenger, flight1);
+            System.out.println("Passengers on the flight to " + flight1.getDestination());
+            for (Passenger person : flight1.getPassengers()) {
+                System.out.println("Passenger name: " + passenger.getName() +
+                        " |  Contact number: " + passenger.getContactInfo() +
+                        " | Unique ID: " + passenger.getUniqueID());
+            }
+        } else {
+            airline.book(passenger, flight2);
+                System.out.println("Passengers on the flight to " + flight2.getDestination());
+                for (Passenger person : flight2.getPassengers()) {
+                    System.out.println("Passenger name: " + passenger.getName() +
+                            " |  Contact number: " + passenger.getContactInfo() +
+                            " | Unique ID: " + passenger.getUniqueID());
+                }
+        }
 
-        
+        System.out.println("Would you like to be removed from this flight? (Y/N) ");
+        String decision = scanner1.nextLine();
+        if (decision.equals("Yes")) {
+            if (destinationName.equals(destination)) {
+                airline.remove(passenger, newFlight);
+                System.out.println("You have been successfully removed from the flight");
+            } else if (destinationName.equals("New York")) {
+                airline.remove(passenger, flight1);
+                System.out.println("You have been successfully removed from the flight");
+            } else {
+                airline.remove(passenger, flight2);
+                System.out.println("You have been successfully removed from the flight");
+            }
+        } else {
+            System.out.println("You remain on the flight");
+        }
 
+        System.out.println("Which flight would you like to cancel? ");
+        String flightCancellation = scanner1.nextLine();
+        if (flightCancellation.equals(destinationName)){
+            airline.cancel(newFlight);
+            System.out.println("The flight has been cancelled");
+        } else if (flightCancellation.equals("New York")){
+            airline.cancel(flight1);
+            System.out.println("The flight has been cancelled");
+        } else {
+            airline.cancel(flight2);
+            System.out.println("The flight has been cancelled");
+        }
 
+        System.out.println("These are the available flights ");
+        ArrayList<Flight> updatedFlights = airline.displayFlights();
+        for (Flight flight : updatedFlights) {
+            System.out.println("Destination: "+ flight.getDestination() +
+                    " |  Flight Number: " + flight.getFlightNumberID());
+        }
     }
-
 }
+
